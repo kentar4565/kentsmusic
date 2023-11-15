8d10354b5f166e0140fa66bbb415c537e6730c95
kentar4565 <121309674+kentar4565@users.noreply.github.com>
2023-11-14 21:42:40 -0600

    main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ intents = discord.Intents.default()
 intents.voice_states = True
 
 bot = Bot(intents=intents)
-bot.run("ODI0ODg5Njc4ODkzMjg1Mzk2.G32C9H.18BKNWCRvOr-GWt-K6J0pygzpBL5KuwSlboWWE")
+bot.run("test")
